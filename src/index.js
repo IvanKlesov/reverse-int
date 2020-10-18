6528517cc7529726fc75db8f7435ff734f64685b
Ivan Klesov <ivklesov@gmail.com>
2020-10-18 11:24:25 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = "";
+    if (n >= 0) {
+        let str = String(n);
+        for (let i = 0; i < str.length; i++) {
+            result = str[i] + result;
+        }
+    } else {
+        let str = String(n);
+        for (let i = 1; i < str.length; i++) {
+            result = str[i] + result;
+        }
+
+    }
+    return +result
 }
